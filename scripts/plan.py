337d039e0273b0ecea76a4a848b627d2fa484641
dalalbhavya <dalalbhavya@gmail.com>
2022-07-18 21:55:01 +0530

    minor changes

--- scripts/plan.py
+++ scripts/plan.py
@@ -2,18 +2,25 @@
 
 import imp
 import rospy
+from trajectory_msgs.msg import JointTrajectoryPoint
 from trajectory_msgs.msg import JointTrajectory
 from control_msgs.msg import JointTrajectoryControllerState
 
 def joint_state_callback(msg):
+    positions = [[0,0,0,0,0,0]]
+    velocities = [[0,0,0,0,0,0]]
+    times = [[1]]
     print("actual = \n", msg.actual)
     joint_state_publisher = rospy.Publisher("/arm_controller/command", JointTrajectory, queue_size=10)
     joint_state_msg = JointTrajectory()
     joint_state_msg.header = msg.header
     joint_state_msg.joint_names = msg.joint_names
-    joint_state_msg.points.positions = [1,1,1,1,1,1]
-    joint_state_msg.points.velocities = [1,1,1,1,1,1]
-        
+    for position, velocity, time in zip(positions, velocities, times):
+        joint_state_point_msg = JointTrajectoryPoint()
+        joint_state_point_msg.positions = position
+        joint_state_point_msg.velocities = velocity
+        joint_state_point_msg.time_from_start.secs = time
+        joint_state_point_msg.time_from_start.nsecs = 0
 
 def main():
     rospy.init_node('joint_state_node', anonymous=True)
